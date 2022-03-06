ae1c3a7e63c1fa1dfe4f28e786c556d77c4570b8
Rich Sharp <rich@therealspring.org>
2022-03-06 11:39:38 -0800

    try to use linear systems -- doesbn't work

--- chemical_equation_balancer.py
+++ chemical_equation_balancer.py
@@ -7,6 +7,10 @@ Represent as
 import re
 import sys
 
+import numpy
+import numpy.linalg
+import scipy.optimize
+
 
 def main():
     equation = ''.join(sys.argv[1:])
@@ -17,19 +21,48 @@ def main():
 
     left, right = equation.split('=')
     left_set, right_set = set(), set()
-    for atom_set, molecule_list in [(left_set, left.split('+')), (right_set, right.split('+'))]:
-        for molecule in molecule_list:
+    left_molecule_list, right_molecule_list = [], []
+    for molecule_list, atom_set, molecule_string in [
+            (left_molecule_list, left_set, left.split('+')),
+            (right_molecule_list, right_set, right.split('+'))]:
+        for molecule in molecule_string:
             molecule_regular_expression = '([A-Z][a-z]?)([1-9]*)'
             atoms = re.findall(molecule_regular_expression, molecule)
+            atom_list = []
             for atom, count in atoms:
+                if count == '':
+                    count = 1
+                else:
+                    count = int(count)
+                atom_list.append((atom, count))
                 atom_set.add(atom)
-        print(f'{molecule}: {atoms}')
+            molecule_list.append(atom_list)
     if left_set-right_set != set():
         print(f'ERROR: there are atoms on the left side that are not on the right side of the equation: {left_set-right_set}')
         return
     if right_set-left_set != set():
         print(f'ERROR: there are atoms on the left side that are not on the right side of the equation: {right_set-left_set}')
-        return
+    print(left_molecule_list)
+    print(right_molecule_list)
+    n_molecules = len(left_molecule_list)+len(right_molecule_list)
+    matrix = numpy.zeros((len(atom_set), n_molecules))
+    # rows should be atoms, columns are molecules
+    for row_index, atom in enumerate(left_set):
+        for offset, sign_factor, molecule_list in [
+                (0, 1, left_molecule_list),
+                (len(left_molecule_list), -1, right_molecule_list)]:
+            for col_index, molecule in enumerate(left_molecule_list):
+                for mol_atom, count in molecule:
+                    if mol_atom == atom:
+                        matrix[row_index, col_index+offset] += count*sign_factor
+    print(atom_set)
+    print(matrix)
+    b = numpy.zeros((n_molecules))
+    c = numpy.ones(n_molecules)
+    constants = scipy.optimize.linprog(c, A_eq=matrix, b_eq=b)
+
+    print(constants)
+            #matrix[row_index, col_index] =
     # chemical_regular_expression = (
     #     '[A-Z]')
 
@@ -38,11 +71,16 @@ def main():
     # look at all molecule factors and simplify out common multiples
 
 
-def calculate_atom_counts(molecule_count):
+def calculate_atom_counts(molecule_list):
     """Calculate the number of atoms given the molecules and their counts.
 
     Args:
-        molecule_count (dict): mapping molecule to integer constant
+        molecule_list (list): list of tuple of the form
+            [(mol_count1, [(atom1, count1), (atom2, count2), ...]),
+             (mol_count2, ....)]
+
+    Returns:
+
 
     """
     pass
