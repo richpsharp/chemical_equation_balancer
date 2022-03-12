e7b60782f328011877ee14feaabc73f70485d605
Rich Sharp <rich@therealspring.org>
2022-03-12 15:24:10 -0800

    adding some state

--- re_test.py
+++ re_test.py
@@ -1,8 +1,31 @@
+from collections import defaultdict
 import re
 
 if __name__ == '__main__':
     token_expression = '([A-Z][a-z]?|[1-9][0-9]*|[\(\)=+])'
-    input_string = 'CO2+H20=C6H12O6+O2'
+    input_string = 'CO2+H20=(CH2O)6+O2'
     tokens = re.findall(token_expression, input_string)
-    print(token_expression)
-    print(tokens)
+    state = 'open'  # open, molecule count
+    molecule_equation = defaultdict(list)
+    side = 'left'
+    molecule = None
+    for token in tokens:
+        if state == 'open':
+            molecule_id = token
+            state = 'molecule_count'
+        elif state == 'molecule_count':
+            state = 'open'
+            try:
+                molecule = (molecule_id, int(token))
+            except ValueError:
+                if token == ')':
+                    print('pop a paren and multiply all counts')
+                else:
+                    # molecule ended
+                    molecule = (molecule_id, 1)
+        if molecule is not None:
+            print(molecule)
+            molecule_equation[side].append(molecule)
+            molecule = None
+        if token == '=':
+            side = 'right'
